5d7206b9a49375ea24869321fd87ccb439674cb9
1dryiwu1 <2489555099@qq.com>
2026-05-31 13:35:51 +0800

    Make GitHub Pages serve built Vue app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: fileURLToPath(new URL('./app.html', import.meta.url)),
+    },
+  },
 });
